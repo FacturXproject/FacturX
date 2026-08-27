d4f66ff52d5625881871d4b1864e185cd983ba33
lasmodis <lasmodis@example.com>
2026-08-27 19:39:52 +0200

    F00 https/tls/nginx

--- scripts/generate-certs.sh
+++ scripts/generate-certs.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-mkdir -p secrets
+mkdir -p nginx/certs
 
 openssl req \
     -x509 \
     -nodes \
     -days 365 \
     -newkey rsa:2048 \
-    -keyout secrets/localhost.key \
-    -out secrets/localhost.crt \
+    -keyout nginx/certs/localhost.key \
+    -out nginx/certs/localhost.crt \
     -subj "/CN=localhost"
 
-echo "TLS certificate generated in secrets/"
+echo "TLS certificate generated in nginx/certs/"
