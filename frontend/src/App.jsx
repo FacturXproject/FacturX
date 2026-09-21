4be61f8ad6bd0b9722e96fc5d4f11cd8af5697af
lasmodis <lasmodis@example.com>
2026-09-21 17:21:03 +0200

    save local changes before merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Processing from './pages/Processing';
 import ComplianceReport from './pages/ComplianceReport';
 import Conversion from './pages/Conversion';
 import Success from './pages/Success';
-import XmlReader from './pages/XmlReader';
+
 import UploadPage from './pages/UploadPage';
 import HealthCheck from './pages/HealthCheck';
 import OrganizationsPage from './pages/OrganizationsPage';
@@ -18,8 +18,7 @@ import Invitations from './pages/Invitations';
 import OrganizationMembersPage from './pages/OrganizationMembersPage';
 import NewInvitation from './pages/NewInvitation';
 import InvitationCheck from './pages/InvitationCheck';
-
-
+import InvoiceView from './pages/InvoiceView';
 
 
 function Protected({ children }) {
@@ -46,11 +45,11 @@ export default function App() {
           <Route path="/invitations/new" element={<Protected><NewInvitation /></Protected>} />
           <Route path="/invitations" element={<Protected><Invitations /></Protected>} />
           <Route path="/invitations/:token" element={<InvitationCheck />} />
+          <Route path="/documents/:id/invoice" element={<Protected><InvoiceView /></Protected>} />
           <Route path="/traitement" element={<Protected><Processing /></Protected>} />
           <Route path="/rapport" element={<Protected><ComplianceReport /></Protected>} />
           <Route path="/conversion" element={<Protected><Conversion /></Protected>} />
           <Route path="/succes" element={<Protected><Success /></Protected>} />
-          <Route path="/lecture-xml" element={<Protected><XmlReader /></Protected>} />
           <Route path="/verifier" element={<Protected><UploadPage mode="verifier" /></Protected>} />
           <Route path="/convertir" element={<Protected><UploadPage mode="convertir" /></Protected>} />
           <Route path="/organisations" element={<Protected><OrganizationsPage /></Protected>} />
